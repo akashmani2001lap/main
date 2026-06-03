166d596d96a9d2a6191952f221b33939ac59e642
demo <akashmani2001am@gmail.com>
2026-06-03 19:19:01 +0530

    updated

---
+++ Js/Day_1/app.js
@@ -0,0 +1,59 @@
+
+
+
+
+// const  Age_limit = (age)=>{
+
+//  const user_Age = age >= 18 ? "You are allowed" : "your not allowed"
+
+//  return user_Age
+
+// }
+
+// console.log(Age_limit(18))
+
+
+
+
+// const Myfunc = (...rest)=>{
+
+//   const EvenNumber = rest.map((e)=> )
+
+//   return Myfunc
+// }
+
+// console.log(Myfunc(12,34,45,30,4,5,6,7))
+
+
+
+const input = (a,b,c,d)=>{
+
+  return a + b + c +d
+
+}
+
+console.log(input(1,2,3,4))
+
+
+
+
+for(i = 0 ; num < i.length; i++){
+
+ i = i + num[i]
+
+ 
+
+}
+
+
+
+
+
+const Myfunc = (...Remind)=>{
+
+
+
+
+
+}
+
