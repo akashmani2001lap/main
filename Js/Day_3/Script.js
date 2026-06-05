6fb0c03b5ce45aabf616bde855f5cbd0058c1fc8
demo <akashmani2001am@gmail.com>
2026-06-05 12:13:54 +0530

    Day_3 task has been updated successfully

--- Js/Day_3/Script.js
+++ Js/Day_3/Script.js
@@ -106,44 +106,141 @@ const atmSystem = (amount) => {
     withdraw(amount) {
       if (amount <= balance) {
         balance -= amount;
-                console.log(`withdrawn amount: ${amount}`);
-
+        console.log(`withdrawn amount: ${amount}`);
       } else {
         console.log("Insuficiant Balance");
-        
       }
     },
 
-    checkBalance(){    
-      console.log(`Balance amount: ${balance}`);     
-    }
+    checkBalance() {
+      console.log(`Balance amount: ${balance}`);
+    },
   };
 };
 
 const atm = atmSystem();
 
-atm.checkBalance()
-atm.withdraw(2000)
-atm.checkBalance()
-
-
+atm.checkBalance();
+atm.withdraw(2000);
+atm.checkBalance();
 
 // Task_6  Login Attempt Tracker
 
-
-const loginTracker = () =>{
-
+const loginTracker = () => {
   let loginAttempt = 0;
 
-  return ()=>{
-    loginAttempt++
-    console.log(`Attempt count:${loginAttempt}`);  
+  return () => {
+    loginAttempt++;
+    console.log(`Attempt count:${loginAttempt}`);
   };
-
 };
 
 const login = loginTracker();
 
-login()
-login()
-login()
+login();
+login();
+login();
+
+// Task 7 - Callback Injection - Payment Gateway
+
+const proccessingPayment = (Callback) => {
+  console.log("Proccessing Payment...");
+
+  Callback();
+};
+
+const GPay = () => {
+  console.log("Payment Successful via GPay");
+};
+
+const PhonePe = () => {
+  console.log("Payment Successful via PhonePe");
+};
+
+const Paytm = () => {
+  console.log("Payment Successful via Paytm");
+};
+
+proccessingPayment(GPay);
+proccessingPayment(PhonePe);
+proccessingPayment(Paytm);
+
+// Task 8 - Callback Injection - User Actions
+
+const executeAction = (cb) => {
+  cb();
+};
+
+const Login = () => {
+  console.log("User Login...");
+};
+
+const Logout = () => {
+  console.log("User Logout...");
+};
+
+const Register = () => {
+  console.log("User Register...");
+};
+
+executeAction(Login);
+executeAction(Logout);
+executeAction(Register);
+
+// Task 9 - setTimeout Notification System
+
+const notificationSystem = ()=>{
+  console.log("Sending Notification...");
+
+  setTimeout(()=>{
+    console.log("Notification Sent"); 
+  },1000)
+
+};
+ notificationSystem ()
+
+
+// Task 10 - Closure + Callback + setTimeout (Combined)
+
+const orderProcessingSystem = ()=>{
+
+  let orderCount = 0;
+
+  return (cb)=>{
+
+    orderCount++
+    console.log(`Order Number : ${orderCount}`);
+          console.log("Processing ...");
+
+    
+    setTimeout(() => {
+      
+      cb();
+      console.log("Order Completed");
+      
+    }, 2000);
+
+  }
+
+}
+
+const mobileOrder = ()=>{
+  console.log("Mobile Order Processed");
+  
+}
+
+const laptopOrder = ()=>{
+  console.log("Laptop Order Processed");
+  
+}
+
+const tvOrder = ()=>{
+  console.log("TV Order Processed");
+  
+}
+
+
+const processOrder = orderProcessingSystem()
+
+processOrder(tvOrder)
+processOrder(laptopOrder)
